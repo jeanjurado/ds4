33935652c237043f451c7526b942f5d963e9a29a
jeanjurado <jean.jurado@utp.ac.pa>
2025-09-28 22:33:05 -0500

    Lab4-2

---
+++ Laboratorio4/Laboratorio4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int i = 1;
+        while (i <= n)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
