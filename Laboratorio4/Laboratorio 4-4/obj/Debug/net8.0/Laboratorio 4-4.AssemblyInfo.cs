be9bd4d81249a9afef17e7e3a94e9b8f77d02ecb
jeanjurado <jean.jurado@utp.ac.pa>
2025-09-28 23:08:22 -0500

    Laboratorio 4-6

--- Laboratorio4/Laboratorio 4-4/obj/Debug/net8.0/Laboratorio 4-4.AssemblyInfo.cs
+++ Laboratorio4/Laboratorio 4-4/obj/Debug/net8.0/Laboratorio 4-4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 4-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5def91f2204c6ca48927aa720bdcb2ce9cb092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e555aadcb753e21d3871747dc81aef72c4d18237")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 4-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 4-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Laboratorio4/Laboratorio 4-6/obj/Debug/net8.0/Laboratorio 4-6.AssemblyInfo.cs
+++ Laboratorio4/Laboratorio 4-6/obj/Debug/net8.0/Laboratorio 4-6.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 4-3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 4-6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e555aadcb753e21d3871747dc81aef72c4d18237")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 4-3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 4-3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 4-6")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 4-6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
